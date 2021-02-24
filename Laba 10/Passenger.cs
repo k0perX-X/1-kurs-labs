ce1cab7a332b74d7e7b6506b4634b48410aad6a0
Николай Перминов <kolya-perminov@ya.ru>
2021-02-24 23:04:59 +0500

    10.3.1 New class

---
+++ Laba 10/Passenger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Laba_10
+{
+    public class Passenger : IInit
+    {
+        private string _station;
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Station
+        {
+            get => _station;
+            set => _station = value;
+        }
+
+        public Passenger()
+        {
+            _station = default;
+            _name = default;
+        }
+
+        private protected static Random random = new Random();
+
+        public virtual object Init()
+        {
+            string[] names = new[] { "Николай", "Игорь", "Никита", "Илья", "Маша", "Марина", "Катя" };
+            string[] st = new[]
+            {
+                "Заводская", "Воздушная", "Хлебозаводская", "Народная", "32 км", "Центральная",
+                "Конечная"
+            };
+            Passenger passenger = new Passenger
+            {
+                Name = names[random.Next(0, names.Length)],
+                Station = st[random.Next(0, st.Length)]
+            };
+            return passenger;
+        }
+
+        public void Show()
+        {
+            Console.Write($"Name: {Name}, Station: {Station}");
+        }
+    }
+}
